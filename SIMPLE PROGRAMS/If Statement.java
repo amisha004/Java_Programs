dc3e55783ff83752f8d61f97e591b8313d43184b
Amisha Sakhare <84026974+amisha004@users.noreply.github.com>
2022-06-17 11:48:32 +0530

    Create If Statement.java

---
+++ SIMPLE PROGRAMS/If Statement.java
@@ -0,0 +1,13 @@
+public class ifSample {
+    public static void main (String args[]){
+        int x,y;
+        x=10;
+        y=20;
+        if (x<y) System.out.println("x is less than y");
+        x=x*2;
+        if (x==y) System.out.println("x is equal to y");
+        x=x*2;
+        if (x>y) System.out.println("x is greater than y");
+        if (x==y) System.out.println("Never execute");
+    }
+}
