dcf07bc1535fb6121c9faf39f9f86001e75ee6eb
Amisha Sakhare <84026974+amisha004@users.noreply.github.com>
2022-06-17 11:25:24 +0530

    Create Boolean type.java

---
+++ SIMPLE PROGRAMS/Boolean type.java
@@ -0,0 +1,14 @@
+public class BoolTest {
+    public static void main (String[] args){
+        boolean b;
+        b=false;
+        System.out.println("b is " +b);
+        b=true;
+        System.out.println("b is " +b);
+
+        if (b) System.out.println("This is executed");
+        b=false;
+        if (b) System.out.println("This is not executed");          //if b is false then 'if' will not execute
+        System.out.println("10 > 9 is " +(10>9));
+    }
+}
